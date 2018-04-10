5b329e8569fae1c89a87bb4fc8bb78f6e336777d
Miles Zimmerman <mileszim@protonmail.com>
2018-04-10 08:24:12 -0700

    Updating plugin to deploy against all targets

--- index.js
+++ index.js
@@ -10,25 +10,39 @@ module.exports = {
     var DeployPlugin = BasePlugin.extend({
       name: options.name,
 
+      defaultConfig: {
+        projectName: function(context) {
+          return context.projectName || context.appName;
+        },
+
+        deployToken: function(context) {
+          return context.deployToken || process.env.FIREBASE_TOKEN || undefined;
+        },
+
+        deployTargets: ['hosting', 'database', 'firestore', 'functions', 'storage']
+      },
+
       upload: function(context) {
-        var outer = this;
-        var project = context.config.firebase.appName || context.config.fireBaseAppName;
+        this.log("Starting deploy process...");
+        var project = this.readConfig('projectName');
+        var build = this.readConfig('outputPath');
+        var message = this.readConfig('revisionKey');
+        var verbose = context.ui.verbose;
+        var token = this.readConfig('deployToken');
+        var targets = this.readConfig('deployTargets');
         var options = {
           project: project,
-          public: context.config.build.outputPath,
-          message: (context.revisionData || {}).revisionKey,
-          verbose: context.ui.verbose
+          public: build, //context.config.build.outputPath,
+          message: message,
+          verbose: verbose,
+          token: token
         };
-        if (context.config.firebase.deployToken || process.env.FIREBASE_TOKEN) {
-          options.token = context.config.firebase.deployToken || process.env.FIREBASE_TOKEN;
-        }
-        return fbTools.deploy(options).then(function() {
-          outer.log('successful deploy!', {verbose: true});
-        }).catch(function(err) {
-          // handle error
-          outer.log('something bad happened oh no', { color: 'red' });
-          outer.log(err, { color: 'red' });
-          outer.log(err.stack, { color: 'red' });
+        return fbTools.deploy(targets, options).then(() => {
+          this.log('Successful deploy!', { color: green });
+        }).catch((err) => {
+          this.log('There was an error during deployment:', { color: 'red' });
+          this.log(err, { color: 'red' });
+          this.log(err.stack, { color: 'red' });
         });
       }
     });
